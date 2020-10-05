4dae8c0a2979c42f6997a0354ea5a99838c0a45c
Raafje16 <34309777+Raafje16@users.noreply.github.com>
2020-10-05 17:10:36 +0200

    minor update

--- projects/SoftwareDesign_by_Nikita_Ravi_and_Jonno/Programs/ArduinoWidget.py
+++ projects/SoftwareDesign_by_Nikita_Ravi_and_Jonno/Programs/ArduinoWidget.py
@@ -9,8 +9,8 @@ from random import randint
 import serial  # import Serial Library
 import numpy  # Import numpy
 
-tempC = []
-pressure = []
+tempC = [0]
+pressure = [0]
 arduinoData = serial.Serial('COM5', 9600)  # Creating our serial object named arduinoData
 class TempSensor(QWidget):
     def __init__(self, parent=None):
@@ -29,8 +29,7 @@ class TempSensor(QWidget):
         layout_monitor.addLayout(layout_monitor_buttons)
 
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(50)
-        self.timer.timeout.connect(self.update_plot_data)
+
 
         self.start_button = QPushButton('Start')
         self.start_button.clicked.connect(self.start_measure)
@@ -48,13 +47,6 @@ class TempSensor(QWidget):
         layout.addWidget(tempWidget)
         layout.addWidget(box_monitor)
 
-        arduinoString = arduinoData.readline()  # read the line of text from the serial port
-        dataArray = arduinoString.split(b',')  # Split it into an array called dataArray
-        temp = float(dataArray[0])  # Convert first element to floating number and put in temp
-        P = float(dataArray[1])  # Convert second element to floating number and put in P
-        tempC.append(temp)  # Build our tempF array by appending temp readings
-        pressure.append(P)  # Building our pressure array by appending P readings
-
         styles = {'color': '#FFFFFF', 'font-size': '16px'}
         tempWidget.setLabel('left', 'Temperature (°C)', **styles)
         tempWidget.setLabel('bottom', 'Time (s)', **styles)
@@ -69,7 +61,9 @@ class TempSensor(QWidget):
 
 
     def start_measure(self):
+        self.timer.setInterval(50)
         if not self.timer.isActive():
+            self.timer.timeout.connect(self.update_plot_data)
             self.timer.start()
 
     def stop_measure(self):
@@ -82,9 +76,7 @@ class TempSensor(QWidget):
         arduinoString = arduinoData.readline()  # read the line of text from the serial port
         dataArray = arduinoString.split(b',')  # Split it into an array called dataArray
         temp = float(dataArray[0])  # Convert first element to floating number and put in temp
-        P = float(dataArray[1])  # Convert second element to floating number and put in P
         tempC.append(temp)  # Build our tempF array by appending temp readings
-        pressure.append(P)  # Building our pressure array by appending P readings
 
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
@@ -112,8 +104,6 @@ class PresSensor(QWidget):
         layout_monitor_buttons = QHBoxLayout()
         layout_monitor.addLayout(layout_monitor_buttons)
         self.timer2 = QtCore.QTimer()
-        self.timer2.setInterval(50)
-        self.timer2.timeout.connect(self.update_plot_data)
 
         self.start_button = QPushButton('Start')
         self.start_button.clicked.connect(self.start_measure)
@@ -131,26 +121,22 @@ class PresSensor(QWidget):
         layout.addWidget(presWidget)
         layout.addWidget(box_monitor)
 
-        arduinoString = arduinoData.readline()  # read the line of text from the serial port
-        dataArray = arduinoString.split(b',')  # Split it into an array called dataArray
-        temp = float(dataArray[0])  # Convert first element to floating number and put in temp
-        P = float(dataArray[1])  # Convert second element to floating number and put in P
-        tempC.append(temp)  # Build our tempF array by appending temp readings
-        pressure.append(P)  # Building our pressure array by appending P readings
-
         styles = {'color': '#FFFFFF', 'font-size': '16px'}
         presWidget.setLabel('left', 'Pressure (bar)', **styles)
         presWidget.setLabel('bottom', 'Time (s)', **styles)
 
-        self.x = list(range(100))  # 100 time points
-        self.y = [0] * 100  # 100 data points
+        self.x1 = list(range(100))  # 100 time points
+        self.y1 = [0] * 100  # 100 data points
 
 
         pen = pg.mkPen(color=(0, 255, 0))
-        self.data_line = presWidget.plot(self.x, self.y, pen=pen)
+        self.data_line = presWidget.plot(self.x1, self.y1, pen=pen)
 
     def start_measure(self):
+        self.timer2.setInterval(50)
+
         if not self.timer2.isActive():
+            self.timer2.timeout.connect(self.update_plot_data)
             self.timer2.start()
 
     def stop_measure(self):
@@ -160,15 +146,13 @@ class PresSensor(QWidget):
     def update_plot_data(self):
         arduinoString = arduinoData.readline()  # read the line of text from the serial port
         dataArray = arduinoString.split(b',')  # Split it into an array called dataArray
-        temp = float(dataArray[0])  # Convert first element to floating number and put in temp
         P = float(dataArray[1])  # Convert second element to floating number and put in P
-        tempC.append(temp)  # Build our tempF array by appending temp readings
         pressure.append(P)  # Building our pressure array by appending P readings
 
-        self.x = self.x[1:]  # Remove the first y element.
-        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+        self.x1 = self.x1[1:]  # Remove the first y element.
+        self.x1.append(self.x1[-1] + 1)  # Add a new value 1 higher than the last.
 
-        self.y = self.y[1:]  # Remove the first
-        self.y.append(P)  # Add new pressure value
+        self.y1 = self.y1[1:]  # Remove the first
+        self.y1.append(P)  # Add new pressure value
 
-        self.data_line.setData(self.x, self.y)  # Update the data.
+        self.data_line.setData(self.x1, self.y1)  # Update the data.
